eeb533b849c6aeac820749440f21453a30faaf51
ihollander <ianholla@gmail.com>
2018-11-02 11:55:19 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,34 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+    set_post!
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
+    set_post!
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    set_post!
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
